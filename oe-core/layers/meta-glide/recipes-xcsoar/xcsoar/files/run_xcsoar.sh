546c20364b49a84ac8f5d25712d994f2a4efc4d8
Andreas Lüthi <andreas.luethi@gmx.net>
2021-02-09 17:38:24 +0100

    TEMP: install kernel module.

--- oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
+++ oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
@@ -5,5 +5,8 @@ ip link set can0 type can bitrate 500000
 ip link set can0 txqueuelen 1000
 ip link set can0 up
 
+### TEMP install kernel module
+insmod /media/sda1/arrowkey.ko
+
 ### run xcsoar
 /opt/XCSoar/bin/xcsoar -fly -datapath=/media/sda1/xcsoar/ -profile=/media/sda1/xcsoar/default.prf
