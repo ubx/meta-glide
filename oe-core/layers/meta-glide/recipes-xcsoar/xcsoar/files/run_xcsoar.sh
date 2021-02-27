2469ca41baa3f4ad030912434f14b921b19ccf65
Andreas Lüthi <andreas.luethi@gmx.net>
2021-02-27 19:30:22 +0100

    The complete startup.

--- oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
+++ oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
@@ -6,7 +6,17 @@ ip link set can0 txqueuelen 1000
 ip link set can0 up
 
 ### TEMP install kernel module
-insmod /media/sda1/arrowkey.ko
+cd /media/sda1/
+insmod arrowkey.ko
+cp xrandr /usr/bin/
+cp libXrandr.so.2 /usr/lib/
+
+echo "Run X, rotate -90 and start XCSoar"
+X -nocursor &
+sleep 1
+
+export DISPLAY=:0
+xrandr -o left
 
 ### run xcsoar
 /opt/XCSoar/bin/xcsoar -portrait -fly -datapath=/media/sda1/xcsoar/ -profile=/media/sda1/xcsoar/default.prf
