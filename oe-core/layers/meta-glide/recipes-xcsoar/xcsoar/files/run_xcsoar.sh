df24b9f91ecbdce0f5dea8c4ea7c7e25f45a9e32
Andreas Lüthi <andreas.luethi@gmx.net>
2021-06-08 13:46:11 +0200

    If inserted, copy XCSoar data to the sd card. Call poweroff instead shutdown.

--- oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
+++ oe-core/layers/meta-glide/recipes-xcsoar/xcsoar/files/run_xcsoar.sh
@@ -12,11 +12,31 @@ sleep 1
 export DISPLAY=:0
 xrandr -o left
 
+### If inserted, copy XCSoar data to the sd card
+SD_MEDIA=/media/mmcblk0p2
+DATAPATH=$SD_MEDIA/xcsoar-data
+
+if mount | grep $SD_MEDIA > /dev/null; then
+   size=$(df -PT $SD_MEDIA | awk 'NR==2 {print int($5)}') 
+   if [ $size -gt 4000000 ];                               
+   then
+      echo "*** Use SD Card for XCSoar Data"
+      if [ ! -d $DATAPATH ]; then
+        mkdir $DATAPATH
+        echo "*** Coyp data to $DATAPATH"
+        cp -r /home/root/.xcsoar/* $DATAPATH
+      fi
+   else
+      echo "*** Use .xcsoar foe XCSoar Data "
+   fi
+fi
+
+
 ### run xcsoar
 sts=-1
 while [ $sts -ne 0 ]
 do
-    /opt/XCSoar/bin/xcsoar -portrait -fly -datapath=/home/root/.xcsoar/
+    /opt/XCSoar/bin/xcsoar -portrait -fly -datapath=$DATAPATH
     echo $?
     sts=$?
     ### echo Keep running on crash
@@ -26,4 +46,4 @@ done
 ### shutdown and power off
 fbi -vt 1 /home/root/xcsoar-640x480-shutdown.ppm
 sleep 3
-shutdown now
+poweroff
